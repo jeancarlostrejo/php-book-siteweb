7146cf995933499d8e9f1654487a1afb8d6c0d89
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-05-24 15:28:48 -0400

    add basic structure for section admin

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administrador del sitio</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+</head>
+<body>
+    <h1>Administrador</h1>
+
+    <script src="../js/bootstrap.bundle.js"></script>
+</body>
+</html>
