74a31d13176408c66a311e8b2c2afe1b0646085e
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-05-24 13:15:18 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Hello world"; 
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio Web</title>
+</head>
+<body>
+    <h1>Sitio web</h1>
+</body>
+</html>
